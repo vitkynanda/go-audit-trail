811663957fad80496a70138e0e807e10724e3e65
Vitky Nanda <vitky@it.sharingvision.com>
2025-04-21 15:00:46 +0700

    update model

--- model/audit_trail.go
+++ model/audit_trail.go
@@ -5,7 +5,7 @@ import (
 )
 
 type AuditTrail struct {
-	ID             uint64    `gorm:"primaryKey;type:UUID;column:id;default:uuid()" json:"id"`
+	ID             string    `gorm:"primaryKey;type:UUID;column:id;default:generateUUIDv4()" json:"id"` // Use generateUUIDv4() for UUID
 	Timestamp      time.Time `gorm:"type:DateTime;column:timestamp;default:now()" json:"timestamp"`
 	RequestURL     string    `gorm:"type:String;column:request_url" json:"request_url"`
 	RequestMethod  string    `gorm:"type:LowCardinality(String);column:request_method" json:"request_method"` // Low cardinality for better compression
@@ -13,7 +13,7 @@ type AuditTrail struct {
 	RequestQuery   string    `gorm:"type:String;column:request_query" json:"request_query"`                   // Raw query string
 	RequestHeaders string    `gorm:"type:String;column:request_headers" json:"request_headers"`               // JSON string
 	RequestBody    string    `gorm:"type:String;column:request_body" json:"request_body"`                     // Raw request body
-	ResponseCode   int       `gorm:"type:Int32;column:response_code" json:"response_code"`
+	ResponseCode   int       `gorm:"type:Int64;column:response_code" json:"response_code"`                    // Use Int32 for better compatibility
 	ResponseStatus string    `gorm:"type:LowCardinality(String);column:response_status" json:"response_status"`
 	UserAgent      string    `gorm:"type:String;column:user_agent" json:"user_agent"`
 	ClientIP       string    `gorm:"type:String;column:client_ip" json:"client_ip"`
